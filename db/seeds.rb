60d1ba74e59195eeb52af795a87697ce46d0b292
Lyron <lyronctk@gmail.com>
2016-04-04 20:39:02 -0700

    Fix sinany

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 if !Restriction.first
 	Restriction.create!(grades: 9, floors:"", full_buildings:"")
 end
+
+person = CvhsLocker.find_by(lastName2: "Sinany")
+person.studentID2 = 311361
+person.save
