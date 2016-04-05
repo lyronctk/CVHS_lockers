9c9ddb98dbed4e150c535a16bb37fc975073fc05
Lyron <lyronctk@gmail.com>
2016-04-04 20:43:00 -0700

    Take back sinany from heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
 if !Restriction.first
 	Restriction.create!(grades: 9, floors:"", full_buildings:"")
 end
-
-person = CvhsLocker.find_by(lastName2: "Sinany")
-person.studentID2 = 311361
-person.save
