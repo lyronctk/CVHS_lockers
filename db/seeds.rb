45a7221d0dadb08ea70452131988de89d7460125
Lyron <lyronctk@gmail.com>
2016-04-06 16:37:04 -0700

    Remove lockers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 if !Restriction.first
 	Restriction.create!(grades: 9, floors:"", full_buildings:"")
 end
+
+counter = 9
+while counter <= 29
+	sum = 1100+counter
+	(LockersDb.find_by locker_id: sum).delete
+	counter++
+end
